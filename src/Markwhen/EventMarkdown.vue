9fe6d1a420bf9db58ce32b1ff234f108b13f691f
xiaoxin <onlinedear@163.com>
2025-02-27 09:33:12 +0800

    Update EventMarkdown.vue

--- src/Markwhen/EventMarkdown.vue
+++ src/Markwhen/EventMarkdown.vue
@@ -14,29 +14,16 @@ const props = defineProps<{
 }>();
 
 const editorOrchestrator = useEditorOrchestratorStore();
-const onChange = (index: number, checked: boolean) => {
-  // This is more wonky than it needs to be
-  // Go through supplemental and get the index in terms of checkboxs of this checkbox
-  // Then match it with the list of ranges we have, and update that range
-  let checkboxIndex = -1;
-  for (let i = 0; i < props.supplemental.length; i++) {
-    const s = props.supplemental[i];
-    if (s.type === "checkbox") {
-      checkboxIndex++;
-      if (index === i) {
-        break;
-      }
-    }
-  }
 
-  const range = (props.matchedListItems as Range[]).filter(
-    (r) => r.type === "checkboxItemIndicator"
-  )[checkboxIndex];
-  if (!range) {
-    console.error("Couldn't find range");
+const onChange = (index: number, checked: boolean) => {
+  // 直接通过 index 找到对应的 Range
+  const range = props.matchedListItems[index];
+  if (!range || range.type !== "checkboxItemIndicator") {
+    console.error("Invalid range or range type");
     return;
   }
 
+  // 更新文本
   editorOrchestrator.setText(`- [${checked ? "x" : " "}]`, {
     from: range.from,
     to: range.to,
@@ -46,10 +33,9 @@ const onChange = (index: number, checked: boolean) => {
 
 <template>
   <div style="font-family: system-ui">
-    <template v-for="(item, index) in supplemental">
+    <template v-for="(item, index) in supplemental" :key="index">
       <div
         v-if="item.type === 'checkbox'"
-        :key="index"
         class="flex flex-row items-center"
       >
         <input
@@ -67,14 +53,12 @@ const onChange = (index: number, checked: boolean) => {
       </div>
       <ul
         v-else-if="item.type === 'listItem'"
-        :key="(item as Block).raw"
         class="list-inside list-disc ml-1"
       >
         <li v-html="(item as Block).raw"></li>
       </ul>
       <p
         v-else-if="item.type === 'text'"
-        :key="'p' + (item as Block).raw"
         v-html="toInnerHtml((item as Block).raw)"
       ></p>
       <img v-else :src="(item as Image).link" class="py-4" />
